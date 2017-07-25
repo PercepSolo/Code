1e88814853781d6510e178b1d05c0818c04580a0
David Morosini <david.morosinineto@gmail.com>
2017-07-25 09:41:56 -0300

    Comentários e pequenas atualizações, aparentemente OK

--- run.cpp
+++ run.cpp
@@ -2,27 +2,42 @@
 #include "Functions/mlp.cpp"
 #include "Functions/parser.cpp"
 
+/*
+	Autor: David Morosini, Gustavo Araújo
+	2017
+
+	Arquivo destinado ao teste do classificador gerado pela rede neural
+*/
+
 vector<vet_float> coleta_teste(void);
 void exibe_vet_main(vet_int r, vect_data_culturas v_c);
 
 int main(int argc, char * argv[]){
-	if(argc != 2){
-		cout << ".run [arq classificador]" << endl;
-	}else{
-		vect_data_culturas v_c = get_data_culturas((char *)"Databases/database_culturas.txt");
 
-		mlp r = get_mlp(argv[1]);
+	//argv[1] = nome do database das culturas
+	//argv[2] = nome do classificador  
+
+	if(argc == 3){
+
+		//carregando as instancias do database das culturas 
+		//para que o resultado seja em forma textual
+		vect_data_culturas v_c = get_data_culturas(argv[1]);
+
+		//carregando o arquivo com o classificador previamente
+		//gerado pelo arquivo fit.cpp
+		mlp r = get_mlp(argv[2]);
+		//os arquivos de teste serão redirecionados pela entrada padrão
+		//função para pegar os dados de teste
 		vector<vet_float> v = coleta_teste();
 		
+		//realizando o teste dos dodados
 		for(int i = 0; i < v.size(); i++){
 			vet_int u = run(r, v[i]);
 			exibe_vet_main(u, v_c);
 		}
-
-		//vet_int u = run(r, v[1]);
-		//exibe_vet_main(u, v_c);
-
-
+	}else{
+		cout << "Parametros incorretos!!" << endl;
+		cout << "./[executavel] [nome Arquivo culturas] [nome arquivo classificador] < [arquivo de exemplos]" << endl;
 	}
 
 
@@ -45,6 +60,7 @@ vector<vet_float> coleta_teste(void){
 	return resp;
 }
 
+//pesquisando na base de dados e exibindo a cultura correta
 void exibe_vet_main(vet_int r, vect_data_culturas v_c){
 	cout << endl;
 	for(int i = 0; i < v_c.size(); i++){
